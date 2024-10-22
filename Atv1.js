36ddb4d04fd2c35978bd78d3ea1dbcd00c5df466
ALUNO SESI PAULISTA <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-10-22 14:53:13 -0300

    .

---
+++ Atv1.js
@@ -0,0 +1,3 @@
+const prompt = require('prompt-sync')();
+var Real = prompt("Qual o valor, em R$, que deseja converter?");
+console.log ("O valor, após convertido para dólar será de US$", Real / 5.5);
