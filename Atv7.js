086f29dddfea4e73a3bcd45420616b3362f34491
Vinícius Rodrigues <vinicius.rodrigues.g.m.pereira@gmail.com>
2024-10-25 10:54:03 -0300

    Update Atv7.js

--- Atv7.js
+++ Atv7.js
@@ -2,4 +2,5 @@ const prompt = require ('prompt-sync')();
 let custo = prompt ("Qual o valor do carro que deseja comprar? ");
 let imposto = custo / 20 * 9;
 let percentual = custo / 4;
-console.log ("O valor final do carro, aplicando os impostos " + imposto.toFixed(2) + ", e o percentual do revendedor " + )
+let total = custo + imposto + percentual;
+console.log ("O valor final do carro, aplicando os impostos " + imposto.toFixed(2) + ", e o percentual do revendedor " + percentual.toFixed(2) + " será de: " + total.toFixed(2) + " reais.");
