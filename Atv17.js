b92b5c9fb56412758dd11fec77d53961d345c3cf
Vinícius Rodrigues <vinicius.rodrigues.g.m.pereira@gmail.com>
2024-11-04 14:30:43 -0300

    Upd

--- Atv17.js
+++ Atv17.js
@@ -1,1 +1,10 @@
 const prompt = require ('prompt-sync')();
+vet = [ ]
+ct = 1
+while (ct <= 15) {
+  vet [ct - 1] = Number(prompt("Digite um número: "))
+  ct++;
+};
+let num = vet.toSorted();
+let vz = vet.filter (num);
+console.log ("O número " + num + " aparece " + vz + " no código.");
